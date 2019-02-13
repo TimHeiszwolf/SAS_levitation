4227818e20baacc4478a49ab18b35d1e95da2a45
Heiszwolf <t.heiszwolf@student.tue.nl>
2019-02-13 14:44:36 +0100

    ADD differentialEquation
    Added the solution to the differential equation which was made by the getStabelizingCurrent.

---
+++ differentialEquationSolution.m
@@ -0,0 +1,12 @@
+%{
+A function which calculates the analytical solution to the differential
+equation made by the getStablizingCurrent function.
+%}
+function distance = differentialEquationSolution(time, startPosition, targetPosition, frictionConstant, accelerationConstant)
+    r1 = ((frictionConstant)^2 - (4*accelerationConstant))^0.5 - frictionConstant;
+    r2 = -((frictionConstant)^2 - (4*accelerationConstant ))^0.5 - frictionConstant;
+    A = (startPosition - targetPosition) * (1 + (r1 / (r2 - r1)));
+    B = -(startPosition - targetPosition) * (r1 / (r2 - r1));
+    
+    distance = A * exp(r1 * time) + B * exp(r2 * time) + targetPosition;
+    
--- runProgram.m
+++ runProgram.m
@@ -2,12 +2,20 @@
 dt = 0.01;% Steps in time of the simulation.
 timeEnd = 15;% End time of the simulation.
 startPosition = -0.01;% Starting position of the ball.
-targetPosition = -0.07;% Target position of the ball.
+targetPosition = -0.04;% Target position of the ball.
 startVelocity = 0.0;% Startin velocity of the ball.
 massBall = 0.019;% The mass of the ball.
 frictionConstant = 1;% The amount of friction (acceleration counteracting the velocity) which prevents large ocilations and makes the system stable.
 accelerationConstant = 1;% The amount of acceleration to reach the target.
 
+%%% Using the numerical method.
 data = simulationMotion(dt, timeEnd, startPosition, targetPosition, startVelocity, massBall, frictionConstant, accelerationConstant);
-
 plotData(data)
+
+%%% Using the differential equation (doesn't take into account the starting velocity).
+time = 0:dt:timeEnd;
+distance = differentialEquationSolution(time, startPosition, targetPosition, frictionConstant, accelerationConstant);
+figure
+plot(time, distance)
+xlabel('Time')
+ylabel('Position')
